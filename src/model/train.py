4530294dde18b5eb4542ce5ef393e7aae7213eff
Eduardo M Sala <edumunozsala@gmail.com>
2023-07-22 12:16:29 +0200

    Updated train.py and job.yml
    Completed the exercise first attemp

--- src/model/train.py
+++ src/model/train.py
@@ -7,12 +7,15 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
+import mlflow
 
+    
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,6 +37,12 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+# Define the split function
+def split_data(df):
+    # Extract features and laberls
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    # Split the dataset into training and test sets
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
